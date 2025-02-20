1600c306c028befa6223d46fe69e5a7543d136ae
adityabhas22 <62494443+adityabhas22@users.noreply.github.com>
2025-02-20 16:57:43 +0530

    Reverted back to original

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add the Streamlit MVP directory to Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'Streamlit MVP'))
-
-# Import the app
-from app import * 
